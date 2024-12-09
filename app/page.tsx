2f0714196841daf1a7dbc2487a2349602f461928
Giorgi Ilariani <codemasterig@gmail.com>
2024-12-09 19:32:41 +0400

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <div className="w-full min-h-screen flex items-center justify-center text-red-400 font-medium text-4xl">
-      ELENE MIYVARXARTTT!
+      Portfolio (Elene Jabanisvhili)
     </div>
   );
 };
