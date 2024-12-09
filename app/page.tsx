e8fe83963c2d9495962cd3daa1c73be9534cadb0
Giorgi Ilariani <codemasterig@gmail.com>
2024-12-09 19:26:29 +0400

    create next app

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center text-red-400 font-medium text-4xl">
+      ELENE MIYVARXARTTT!
+    </div>
+  );
+};
+
+export default page;
