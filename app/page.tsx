033b8fd0306476e6dbfbbd02f2277c10b324a35e
Giorgi Ilariani <codemasterig@gmail.com>
2024-12-09 19:36:35 +0400

    change text

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="w-full min-h-screen flex items-center justify-center text-red-400 font-medium text-4xl">
-      Portfolio (Elene Jabanisvhili)
+    <div className="w-full min-h-screen flex items-center justify-center">
+      <div className="flex flex-col gap-6 text-center">
+        <p className="font-medium text-4xl">Portfolio</p>
+        <p className="font-medium text-4xl">Elene Jabanishvili</p>
+      </div>
     </div>
   );
 };
